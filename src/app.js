445cdf8272d6b190bd3e3d3eba8f3efe492b7785
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-12-19 15:16:58 +0100

    trying to setup deployment

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*', credentials: true }));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello');
 });
 
