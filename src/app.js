38b6b24ab367dddeeaea7f428531602557dce8d9
TIMO NICOLAUX <timo.nicolaux@gmail.com>
2022-12-19 15:05:23 +0100

    modidied headers in vercel file

--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@ const cors = require('cors');
 const db = require('./db');
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+app.use(cors({ origin: '*', credentials: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello');
